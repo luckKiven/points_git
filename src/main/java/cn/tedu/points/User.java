3f2c86faef5ffbe5ff42be1a2a5bab6f526ece5c
tedu-uid <2213593288@qq.com>
2019-08-20 11:12:40 +0800

    v1.02

--- src/main/java/cn/tedu/points/User.java
+++ src/main/java/cn/tedu/points/User.java
@@ -3,6 +3,7 @@ package cn.tedu.points;
 public class User {
     private String name;
     private Integer age;
+    private Integer salary;
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
