fdb8f318f08fd2e4c8398057d4ece346703399f2
kiven <221359@qq.com>
2019-08-26 22:27:42 +0800

    v1.04

--- src/main/java/cn/tedu/points/User.java
+++ src/main/java/cn/tedu/points/User.java
@@ -5,6 +5,7 @@ public class User {
     private Integer age;
     private Integer salary;
     private String name3;
+    private String salary1;
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
